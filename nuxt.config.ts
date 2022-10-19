b9beb99492f92fe8a22dd37bc31a36a63d62bcfb
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-10-19 15:44:10 -0400

    static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,19 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    css: ['~/assets/main.css'],
-    modules: [
-        // ...
-        [
-          '@pinia/nuxt',
-          {
-            autoImports: [
-              // automatically imports `defineStore`
-              'defineStore', // import { defineStore } from 'pinia'
-              // automatically imports `defineStore` as `definePiniaStore`
-              ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
-            ],
-          },
+  target: "static",
+  css: ["~/assets/main.css"],
+  modules: [
+    // ...
+    [
+      "@pinia/nuxt",
+      {
+        autoImports: [
+          // automatically imports `defineStore`
+          "defineStore", // import { defineStore } from 'pinia'
+          // automatically imports `defineStore` as `definePiniaStore`
+          ["defineStore", "definePiniaStore"], // import { defineStore as definePiniaStore } from 'pinia'
         ],
-      ],
+      },
+    ],
+  ],
 });
