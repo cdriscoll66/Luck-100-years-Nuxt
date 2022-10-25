a200e871a101fa452d0ed7344684e5ab8d7b0a57
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-10-25 16:21:59 -0400

    your stories examples

--- pages/your-stories/index.vue
+++ pages/your-stories/index.vue
@@ -1,38 +1,84 @@
 <script setup lang="ts">
 import yourstories from '~/content/yourstories.json'
-
-
 </script>
 
 <template>
   <div class="wrapper">
     <YourStoriesHero />
-      <div class="yourstories__archive">
-        <div v-for="story, key in yourstories" class="yourstories__card" :key="key">
-            <CardsLargeCard v-if="story.cardtype === 'full'" :title=story.title :type=story.type :image=story.image :subtitle=story.subtitle :tagline=story.tagline :link=story.link />
-            <CardsTextMediaCard v-else-if="story.cardtype === 'textmedia'"  :title=story.title :type=story.type :image=story.image :subtitle=story.subtitle :tagline=story.tagline :link=story.link  />
-            <CardsProfileImageCenterCard v-else-if="story.cardtype === 'profileimagecenter'"  :title=story.title :type=story.type :image=story.image :subtitle=story.subtitle :tagline=story.tagline :link=story.link  />
-            <CardsProfileImageLeftCard v-else-if="story.cardtype === 'profileimageleft'"  :title=story.title :type=story.type :image=story.image :subtitle=story.subtitle :tagline=story.tagline :link=story.link  />
-            <CardsSmallShadowLeftCard v-else-if="story.cardtype === 'smallshadowleft'"  :title=story.title :type=story.type :subtitle=story.subtitle :tagline=story.tagline :link=story.link  />
-            <CardsSmallShadowRightCard v-else-if="story.cardtype === 'smallshadowright'"  :title=story.title :type=story.type :subtitle=story.subtitle :tagline=story.tagline :link=story.link  />
-            <div v-else>{{ story.title }}</div>
-        </div>
+    <div class="yourstories__archive">
+      <div
+        v-for="(story, key) in yourstories"
+        class="yourstories__card"
+        :key="key"
+      >
+        <CardsLargeCard
+          v-if="story.cardtype === 'full'"
+          :title="story.title"
+          :type="story.type"
+          :image="story.image"
+          :subtitle="story.subtitle"
+          :tagline="story.tagline"
+          :link="story.link"
+        />
+        <CardsTextMediaCard
+          v-else-if="story.cardtype === 'textmedia'"
+          :title="story.title"
+          :type="story.type"
+          :image="story.image"
+          :subtitle="story.subtitle"
+          :tagline="story.tagline"
+          :link="story.link"
+        />
+        <CardsProfileImageCenterCard
+          v-else-if="story.cardtype === 'profileimagecenter'"
+          :title="story.title"
+          :type="story.type"
+          :image="story.image"
+          :subtitle="story.subtitle"
+          :tagline="story.tagline"
+          :link="story.link"
+        />
+        <CardsProfileImageLeftCard
+          v-else-if="story.cardtype === 'profileimageleft'"
+          :title="story.title"
+          :type="story.type"
+          :image="story.image"
+          :subtitle="story.subtitle"
+          :tagline="story.tagline"
+          :link="story.link"
+        />
+        <CardsSmallShadowLeftCard
+          v-else-if="story.cardtype === 'smallshadowleft'"
+          :title="story.title"
+          :type="story.type"
+          :subtitle="story.subtitle"
+          :tagline="story.tagline"
+          :link="story.link"
+        />
+        <CardsSmallShadowRightCard
+          v-else-if="story.cardtype === 'smallshadowright'"
+          :title="story.title"
+          :type="story.type"
+          :subtitle="story.subtitle"
+          :tagline="story.tagline"
+          :link="story.link"
+        />
+        <div v-else>{{ story.title }}</div>
       </div>
+    </div>
 
     <NuxtPage />
-  <PrefooterCta />
+    <PrefooterCta />
   </div>
 </template>
 
 <style lang="scss">
-
 .wrapper {
   padding-top: 79px;
   max-width: 1120px;
   width: 100%;
   margin: 0 auto;
 }
-
 
 .eyebrow {
   font-weight: 700;
@@ -67,41 +113,119 @@ import yourstories from '~/content/yourstories.json'
 }
 
 .subtitle {
-    font-weight: 500;
+  font-weight: 500;
 }
 
 .tagline {
-    font-weight: 300;
+  font-weight: 300;
 }
 
 a.titlelink {
-    display: flex;
-    flex-flow: row nowrap;
-    align-items: center;
-    h3 {
-        font-style: italic;
-        font-size: 3rem;
-        line-height: 1.1666666666666667;
-        transition: color 0.2s ease-in-out;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  h3 {
+    font-style: italic;
+    font-size: 3rem;
+    line-height: 1.1666666666666667;
+    transition: color 0.2s ease-in-out;
 
-        &:hover {
-            color: var(--color-green-dark);
-        }
+    &:hover {
+      color: var(--color-green-dark);
     }
-    &:after {
-        content: "";
-        display: inline-block;
-        margin-left: 12px;
-        height: 12px;
-        width: 20px;
-        background-image: url("/images/arrow.svg");
-        background-repeat: no-repeat;
-        background-size: contain;
-        transition: margin-left 0.2s ease-in-out;
-    }
-    &:hover:after {
-      margin-left:20px;      
-    }
+  }
+  &:after {
+    content: '';
+    display: inline-block;
+    margin-left: 12px;
+    height: 12px;
+    width: 20px;
+    background-image: url('/images/arrow.svg');
+    background-repeat: no-repeat;
+    background-size: contain;
+    transition: margin-left 0.2s ease-in-out;
+  }
+  &:hover:after {
+    margin-left: 20px;
+  }
 }
 
+.story__content {
+  width: 100%;
+  max-width: 740px;
+  margin: 50px auto 50px 50px;
+  p {
+    margin-top: 26px;
+    font-weight: 200;
+    font-size: 1rem;
+    line-height: 1.62;
+    color: #595959;
+  &.statement {
+    font-family: 'Sentinel', serif;
+    font-size: 2rem;
+    line-height: 1.4375;
+    color: var(--color-gray);
+    font-style: italic;
+  }
+  }
+}
+
+.story__statement {
+  margin: 50px auto;
+  max-width: 920px;
+}
+
+
+
+.story {
+  min-height: 100vh;
+  padding:  80px 0 160px;
+  width: 100%;
+  max-width: 1060px;
+  margin: 0 auto;
+}
+
+.title-block {
+  width: 100%;
+  max-width: 960px;
+  margin: 0 auto;
+}
+
+.title-block__inner {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  margin-top: 100px;
+  margin-bottom: 100px;
+  gap: 80px;
+}
+.pagination-links {
+  display: flex;
+  justify-content: space-between;
+  align-items: stretch;
+  margin-top: 200px;
+  position: absolute;
+  bottom: 20px;
+  left: 20px;
+  right: 20px;
+  text-align: center;
+}
+
+.pagination-links a.next,
+.pagination-links a.prev {
+  border: 1px solid var(--color-gold-light);
+  width: 100%;
+  padding: 50px 10px;
+  color: var(--color-green);
+  font-weight: 300;
+  text-transform: uppercase;
+  letter-spacing: 0.2em;
+  background-color: var(--color-off-white);
+}
+
+.pagination-links a.next:hover,
+.pagination-links a.prev:hover {
+  background-color: var(--color-gold-light);
+}
 </style>
--- pages/your-stories/va-tech.vue
+++ pages/your-stories/va-tech.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 
 
+
 </script>
 
 <template>
@@ -19,14 +20,8 @@
       </div>
     </div>
 
-    <div class="video">
-      <video id="vid" ref="vid" controls autoplay playsinline preload="auto">
-        <source src="/video/test.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-    </div>
-
-
+  
+    <VidPlayer vid="/video/test.mp4" />
     <div class="story__content">
    <p>Magna pars studiorum, prodita quaerimus. Etiam habebis sem dicantur magna mollis euismod. Non equidem invideo, miror magis posuere velit aliquet. Contra legem facit qui id facit quod lex prohibet.</p>    <p>Pellentesque habitant morbi tristique senectus et netus. Nec dubitamus multa iter quae et nos invenerat. Quis aute iure reprehenderit in voluptate velit esse.</p>    <p>Me non paenitet nullum festiviorem excogitasse ad hoc. Sed haec quis possit intrepidus aestimare tellus. Curabitur blandit tempus ardua ridiculus sed magna. Ut enim ad minim veniam, quis nostrud exercitation. Fictum, deserunt mollit anim laborum astutumque! Ab illo tempore, ab est sed immemorabili.</p>    <p>Quid securi etiam tamquam eu fugiat nulla pariatur. Cum sociis natoque penatibus et magnis dis parturient. Mercedem aut nummos unde unde extricat, amaras.</p>    <p>Magna pars studiorum, prodita quaerimus. Etiam habebis sem dicantur magna mollis euismod. Non equidem invideo, miror magis posuere velit aliquet. Contra legem facit qui id facit quod lex prohibet.</p>    <p>Pellentesque habitant morbi tristique senectus et netus. Nec dubitamus multa iter quae et nos invenerat. Quis aute iure reprehenderit in voluptate velit esse.</p>    <p>Me non paenitet nullum festiviorem excogitasse ad hoc. Sed haec quis possit intrepidus aestimare tellus. Curabitur blandit tempus ardua ridiculus sed magna. Ut enim ad minim veniam, quis nostrud exercitation. Fictum, deserunt mollit anim laborum astutumque! Ab illo tempore, ab est sed immemorabili.</p>    <p>Quid securi etiam tamquam eu fugiat nulla pariatur. Cum sociis natoque penatibus et magnis dis parturient. Mercedem aut nummos unde unde extricat, amaras.</p> ",
 
@@ -39,91 +34,10 @@
       <NuxtLink to="/your-stories" class="prev">
         <span>Back To All Stories</span>
       </NuxtLink>
-      <NuxtLink class="next" to="yourstories/scott-claud">
+      <NuxtLink class="next" to="/your-stories/scott-claud">
         <span>Next Story</span>
       </NuxtLink>
     </div>
 </div>
 </template>
 
-<style lang="scss" scoped>
-.story__content {
-    width: 100%; 
-    max-width: 740px;
-    margin: 50px auto 50px 50px;
-p {
-    margin-top: 26px;
-    font-weight: 200;
-    font-size: 1rem;
-    line-height: 1.62;
-    color: #595959;
-}
-}
-.video {
-  margin-top: 56px;
-  video {
-    width: 100%;
-  }
-}
-
-.story {
-  min-height: 100vh;
-  padding: 160px 0;
-  width: 100%;
-  max-width: 1060px;
-  margin: 0 auto;
-}
-.title-block {
-  width: 100%;
-  max-width: 960px;
-  margin: 0 auto;
-}
-
-.title-block__inner {
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: center;
-    margin-top: 100px;
-    margin-bottom: 100px;
-    gap: 80px;
-}
-.pagination-links {
-  display: flex;
-  justify-content: space-between;
-  align-items: stretch;
-  margin-top: 200px;
-  position: absolute;
-  bottom: 20px;
-  left: 20px;
-  right: 20px;
-  text-align: center;
-}
-
-.pagination-links a.next,
-.pagination-links a.prev {
-  border: 1px solid var(--color-gold-light);
-  width: 100%;
-  padding: 50px 10px;
-  color: var(--color-green);
-  font-weight: 300;
-  text-transform: uppercase;
-  letter-spacing: 0.2em;
-  background-color: var(--color-off-white);
-}
-
-.pagination-links a.next:hover,
-.pagination-links a.prev:hover {
-  background-color: var(--color-gold-light);
-}
-</style>
-
-<style>
-.story__content p {
-    margin-top: 26px;
-    font-weight: 200;
-    font-size: 1rem;
-    line-height: 1.62;
-    color: #595959;
-}
-</style>
