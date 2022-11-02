1ab7518c51307f654e5e28e2580210f8a938d9a1
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-11-02 12:34:20 -0400

    four gens system build

--- pages/four-generations/[link].vue
+++ pages/four-generations/[link].vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-
+import { Block } from '@babel/types'
 import fourgenerations from '~/content/fourgenerations.json'
 
 const route = useRoute()
@@ -7,12 +7,140 @@ const route = useRoute()
 const profile = computed(() => {
   return fourgenerations.find((profile) => profile.link === route.params.link)
 })
-
 </script>
 
 <template>
-<h1>{{ profile.title }}</h1>
+  <div class="bg-wrapper__profile">
+    <div class="wrapper">
+      <div class="profile">
+        <div class="profile__image">
+          <img :src="`/images/${profile.image}`" :alt="profile.title" />
+        </div>
+        <div class="profile__text">
+          <h2>{{ profile.title }}</h2>
+          <p class="quote">{{ profile.quote }}</p>
+        </div>
+      </div>
+
+      <div class="intro">
+        <p v-for="paragraph in profile.intro" :key="paragraph">
+          {{ paragraph }}
+        </p>
+      </div>
+
+      <div class="biography">
+        <div
+          v-for="(block, key) in profile.bio_blocks"
+          :key="key"
+          :class="['bio', block.portrait ? 'portrait' : 'landscape']"
+        >
+          <figure class="biography__image">
+            <ZoomImage :src="`fourgens/${block.image}`" :alt="block.alt" />
+            <figcaption class="caption">{{ block.caption }}</figcaption>
+          </figure>
+          <AudioPlayer v-if="block.audio" :audio="`/audio/${block.audio}`" />
+        </div>
+      </div>
+      <PrefooterCta />
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+.profile {
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  margin: 20px;
+  padding: 50px 20px;
+  gap: 80px;
+  max-width: 960px;
+  @media (min-width: 900px) {
+    flex-flow: row nowrap;
+    width: 100%;
+    margin: 120px auto 0 0;
+  }
+  h2 {
+    font-style: italic;
+    font-size: 40px;
+    line-height: 1.2;
+    margin-bottom: 16px;
+  }
+  p.quote {
+    font-family: 'Professor', serif;
+    font-size: 3rem;
+    line-height: 1.04;
+    color: var(--color-gray);
+    margin-bottom: 32px;
+  }
+  &:nth-child(even) {
+    justify-content: flex-end;
+  }
+}
+
+.intro {
+  width: 100%;
+  max-width: 740px;
+
+  p {
+    font-weight: 200;
+    line-height: 1.62;
+    color: var(--color-gray-cool);
+    margin-top: 16px;
+  }
+}
+
+div.biography {
+  display: flex;
+  flex-flow: column nowrap;
+  margin: 80px 10px;
+}
+div.bio {
+  margin-top: 48px;
+  &:nth-child(even) {
+    align-self: flex-end;
+  }
+}
+
+div.portrait {
+  width: 100%;
+  max-width: 466px;
+}
+
+div.landscape {
+  width: 100%;
+  max-width: 738px;
+}
+
+figcaption.caption {
+  font-size: 0.75rem;
+  font-weight: 200;
+  line-height: 1.25;
+  color: var(--color-gray);
+  margin: 13px auto 0;
+  width: 100%;
+  max-width: 405px;
+  text-align: center;
+  &:after {
+    content: '';
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    background-image: url('/images/volume.svg');
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    margin: 0 0 0 8px;
+    position: absolute;
+  }
+}
+
+.bg-wrapper__profile {
+  background-color: var(--color-off-white);
+  background-image: 
+    url('/images/topography-single.png'), url('/images/prefooter1.png'),
+    url('/images/prefooter2.png');
+  background-repeat: no-repeat;
+  background-position:  100% 200px, 0 100%, 100% 100%;
+}
 </style>
--- pages/four-generations/index.vue
+++ pages/four-generations/index.vue
@@ -1,38 +1,68 @@
 <script setup lang="ts">
 import fourgenerations from '~/content/fourgenerations.json'
-
 </script>
 
 <template>
-
-<div class="wrapper">
-    <div class="introblock">
+  <div class="bg-wrapper__fourgens">
+    <div class="wrapper">
+      <div class="introblock">
         <div class="introblock__inner">
-            <h1>Four Generations</h1>
-            <h3>Four generations of family leaders have built a business on the idea that every single one of us has the potential to be a leader, that if we live and lead in alignment with core values, we can all make a positive impact on the lives of other people. The importance of people and culture. A commitment to innovation and constant reinvention. An orientation toward the customer and the market. And at the core of it all-family.</h3>
-            <p>These simple and powerful ideas have been passed down from generation to generation, providing consistency and clarity of purpose for the true authors of our history–the people we are honored to call our associates. It is their leadership, their commitment, their creativity and their integrity that have made it all possible, and will continue to do so for generations to come.</p>
+          <h1>Four Generations</h1>
+          <h3>
+            Four generations of family leaders have built a business on the idea
+            that every single one of us has the potential to be a leader, that
+            if we live and lead in alignment with core values, we can all make a
+            positive impact on the lives of other people. The importance of
+            people and culture. A commitment to innovation and constant
+            reinvention. An orientation toward the customer and the market. And
+            at the core of it all-family.
+          </h3>
+          <p>
+            These simple and powerful ideas have been passed down from
+            generation to generation, providing consistency and clarity of
+            purpose for the true authors of our history–the people we are
+            honored to call our associates. It is their leadership, their
+            commitment, their creativity and their integrity that have made it
+            all possible, and will continue to do so for generations to come.
+          </p>
         </div>
+      </div>
+      <div class="profiles">
+        <div v-for="(profile, key) in fourgenerations" class="profile">
+          <div class="profile__image">
+            <img :src="`/images/${profile.image}`" :alt="profile.title" />
+          </div>
+          <div class="profile__text">
+            <h2>{{ profile.title }}</h2>
+            <p class="quote">{{ profile.quote }}</p>
+            <NuxtLink
+              class="link-button"
+              :to="`/four-generations/${profile.link}`"
+            >
+              Explore His Story
+            </NuxtLink>
+          </div>
+        </div>
+      </div>
+      <PrefooterCta />
     </div>
-    <div class="profiles">
-        <div v-for="profile, key in fourgenerations" class="profile">
-            <div class="profile__image">
-                <img :src="`/images/${profile.image}`" :alt="profile.title" />
-            </div>
-            <div class="profile__text">
-                <h2>{{ profile.title }}</h2>
-                <p class="quote">{{ profile.quote }}</p>
-                <NuxtLink class="link-button" :to="`/four-generations/${profile.link}`">Explore His Story</NuxtLink>
-            </div>
-        </div> 
-    </div>
-
-</div>
-
-
-</template> 
-
+  </div>
+</template>
 
 <style lang="scss" scoped>
+.bg-wrapper__fourgens {
+  background-color: var(--color-off-white);
+  background-image: 
+  url('/images/topography-4g-tl.png'),
+  url('/images/topography-4g-tr.png'),
+  url('/images/geology-4g-ml.png'),
+  url('/images/geology-4g-mr.png'),
+  url('/images/geology-4g-ml2.png'),
+    url('/images/prefooter1.png'),
+    url('/images/prefooter2.png');
+  background-repeat: no-repeat;
+  background-position: 0 100px, 100% 200px, 0 35%, 100% 60%, 0 75%, 0 100%, 100% 100%;
+}
 
 .introblock {
   max-width: 960px;
@@ -46,56 +76,63 @@ import fourgenerations from '~/content/fourgenerations.json'
 }
 
 h1 {
-    font-family: 'Sentinel', serif;
-    font-style: italic;
-    font-weight: 300;
-    font-size: 5.375rem;
-    line-height: 1.2;
+  font-family: 'Sentinel', serif;
+  font-style: italic;
+  font-weight: 300;
+  font-size: 5.375rem;
+  line-height: 1.2;
 }
 
 h3 {
-    font-family: 'Sentinel', serif;
-    font-style: italic;
-    font-weight: 200;
-    font-size: 1.5;
-    line-height: 1.54;
-    color: var(--color-gray);
+  font-family: 'Sentinel', serif;
+  font-style: italic;
+  font-weight: 200;
+  font-size: 1.5rem;
+  line-height: 1.54;
+  color: var(--color-gray);
+  margin-bottom: 24px;
 }
 
 p {
-    font-family: 'Gotham HTF', sans-serif;
-    font-weight: 200;
-    line-height: 1.625;
+  font-family: 'Gotham HTF', sans-serif;
+  font-weight: 200;
+  line-height: 1.625;
 }
 
 .profile {
-    display: flex;
-    flex-flow: column nowrap;
-    align-items: center;
-    margin: 20px;
-    padding: 50px 0;
-    gap: 80px;
-    @media (min-width: 900px) {
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  margin: 20px;
+  padding: 50px 10px;
+  gap: 80px;
+  @media (min-width: 768px) {
     flex-flow: row nowrap;
-      width: 100%;
-      margin: 120px auto 0;
-    }
-    h2 {
-        font-style: italic;
-        font-size: 40px;
-        line-height: 1.2;
-        margin-bottom: 16px;
-    }
-    p.quote {
-        font-family: 'Professor', serif;
-        font-size: 3rem;
-        line-height: 1.04;
-        color: var(--color-gray);
-        margin-bottom: 32px;
-        
-    }
-    &:nth-child(even) {
-        justify-content: flex-end;
+    width: 100%;
+    margin: 120px auto 0;
+  }
+  h2 {
+    font-style: italic;
+    font-size: 40px;
+    line-height: 1.2;
+    margin-bottom: 16px;
+  }
+  p.quote {
+    font-family: 'Professor', serif;
+    font-size: 3rem;
+    line-height: 1.04;
+    color: var(--color-gray);
+    margin-bottom: 32px;
+  }
+  &:nth-child(even) {
+    justify-content: flex-end;
+  }
+}
+
+.profile__text {
+    align-self: flex-start;
+    @media (min-width: 768px) {
+        align-self: center;
     }
 }
-</style>   
+</style>
